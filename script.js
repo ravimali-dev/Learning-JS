4eaee1cd44fbae4209bbdecd514ae274a86bdd8b
Ravi Mali <ravimalakar091@gmail.com>
2026-03-04 12:20:58 +0530

    practis javascripy

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-for(let i = 1; i<=10; i++){
-    console.log(i)
+// Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
+
+
+
+
+
+
+
+
+
+function removChar(str){
+
+   let result = str.slice(0,-3);
+   return result;
 }
+console.log(removChar("asdfgg"))
