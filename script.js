cf83cd1ffbb67fd033c80fd05ba5e071435c0bbc
Ravi Mali <ravimalakar091@gmail.com>
2026-03-09 18:00:25 +0530

    solove array question

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-// Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
+//Count even and odd numbers
 
+let arr = [1,2,3,4,5,6];
+let even = 0;
+let odd = 0;
 
-
-
-
-
-
-
-
-function removChar(str){
-
-   let result = str.slice(0,-3);
-   return result;
+for(let i=0; i<arr.length; i++){
+    if(i%2==0){
+        even++;
+    }else{
+        odd++
+    }
 }
-console.log(removChar("asdfgg"))
+// console.log(even);
+// console.log(odd);
+
+//Find average of array
+
+let arr1 = [2,4,6,8];
+let average;
+let sum = 0;
+
+for(let i=0; i<arr1.length; i++){
+     sum += arr1[i];
+    average = sum/arr1.length;
+}
+console.log(average);
+
+//Count how many times a number appears
+
+let arr2 = array = [1,2,2,3,2,4];
+let target = 2;
+let numAppear = 0;
+for(let i=0; i<arr2.length; i++){
+    if(arr2[i]==target){
+        numAppear++
+    }
+}
+console.log(numAppear);
