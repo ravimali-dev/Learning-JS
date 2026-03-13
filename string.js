189f96a2357b8d55ad6412529df5c0ca78c69b78
Ravi Mali <ravimalakar091@gmail.com>
2026-03-13 22:52:12 +0530

    solov a string question a string is palindrone or not

--- string.js
+++ string.js
@@ -1,20 +1,39 @@
 //Given a string s, return the reverse of the string.
-let s = "hello";
+// let s = "hello";
 
 
-let arr = s.split("");
+// let arr = s.split("");
+
+// let left = 0;
+// let right = arr.length - 1;
+
+// while (left < right) {
+//     let temp = arr[left];
+//     arr[left] = arr[right];
+//     arr[right] = temp;
+
+//     left++;
+//     right--;
+// }
+
+// console.log(arr.join(""));
+
+//string is palindrome or not
+
+let s = "Ravi";
 
 let left = 0;
-let right = arr.length - 1;
+let right = s.length - 1;
+let isPalindrome = true;
 
 while (left < right) {
-    let temp = arr[left];
-    arr[left] = arr[right];
-    arr[right] = temp;
+    if (s[left] !== s[right]) {
+        isPalindrome = false;
+        break;
+    }
 
     left++;
     right--;
 }
 
-console.log(arr.join(""));
-
+console.log(isPalindrome);
