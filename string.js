58285da071630b92458b6cf6cb47f1542ad2b9b4
Ravi Mali <ravimalakar091@gmail.com>
2026-03-13 22:59:19 +0530

    Given a string s, find how many times each character appears in the string.

--- string.js
+++ string.js
@@ -20,20 +20,38 @@
 
 //string is palindrome or not
 
-let s = "Ravi";
+// let s = "Ravi";
 
-let left = 0;
-let right = s.length - 1;
-let isPalindrome = true;
+// let left = 0;
+// let right = s.length - 1;
+// let isPalindrome = true;
 
-while (left < right) {
-    if (s[left] !== s[right]) {
-        isPalindrome = false;
-        break;
+// while (left < right) {
+//     if (s[left] !== s[right]) {
+//         isPalindrome = false;
+//         break;
+//     }
+
+//     left++;
+//     right--;
+// }
+
+// console.log(isPalindrome);
+
+//Given a string s, find how many times each character appears in the string.
+
+
+let s = "banana";
+let freq = {};
+
+for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+
+    if (freq[char]) {
+        freq[char]++;
+    } else {
+        freq[char] = 1;
     }
-
-    left++;
-    right--;
 }
 
-console.log(isPalindrome);
+console.log(freq);
